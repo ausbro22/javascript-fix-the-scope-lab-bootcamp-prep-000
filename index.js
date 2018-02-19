1a9e0efe51529e28f7bca133659c6a55aa063f93
Austin Browne <aus.bro22@gmail.com>
2018-02-19 22:10:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 
+var animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+var animal = "cat";
+  return animal
+}
+
+function add2(n) {
+  const two = 2
+  return n + 2
 
   // Feel free to move things around!
   const two = 2
